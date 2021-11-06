570e5804ecdb1b04d825c1dc5606e94593347876
JUANRIVERA2021CODER <93807812+JUANRIVERA2021CODER@users.noreply.github.com>
2021-11-06 14:03:05 -0500

    Create ComputeArea.cpp

---
+++ c++/liang/ComputeArea.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double radius;
+	double area;
+
+	// read in radius
+	radius = 20;
+	// Compute area
+	area = radius * radius * 3.14159;
+	// Display the area
+	cout << "The area is " << area << endl;
+
+	return 0;
+}

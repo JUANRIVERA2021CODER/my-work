f28716d0ad38ea6eb817c5e0cfa904d6d8334e15
JUANRIVERA2021CODER <93807812+JUANRIVERA2021CODER@users.noreply.github.com>
2021-11-08 19:18:24 -0600

    Create gcd.cpp

---
+++ c++/gcd.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int, int);
+
+int main()
+{
+	int num1, num2;
+
+	cout << "Enter two integer: ";
+	cin >> num1 >> num2;
+
+	cout << "The greatest common divisor of " << num1;
+	cout << " and " << num2 << " is ";
+	cout << gcd(num1, num2) << endl;
+
+	return 0;
+}
+
+int gcd(int x, int y)
+{
+	if (x % y == 0)
+		return y;
+	else
+		return gcd(y, x % y);
+}
